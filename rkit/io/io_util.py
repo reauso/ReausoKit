d282e25e891c3030ce78b75f98ddacc79167df0d
reauso <r.ebertowski@gmx.de>
2023-10-05 15:35:42 +0200

    added io io_util.py file

---
+++ rkit/io/io_util.py
@@ -0,0 +1,19 @@
+import os
+
+import glob
+
+
+# TODO add other methods from projects
+
+# TODO test
+def files_in_directory(directory_path, file_patterns=None, recursive=False):
+    if file_patterns is None:
+        file_patterns = ['**']
+    elif not isinstance(file_patterns, list):
+        file_patterns = [file_patterns]
+
+    files = []
+    for pattern in file_patterns:
+        files.extend(glob.glob(os.path.join(directory_path, pattern), recursive=recursive))
+
+    return files
